2084a4f7eacb0121ec186ba9f754344b071f1169
Khorda <fla.cordari@stud.uniroma3.it>
2014-04-12 17:21:49 +0200

    commit3

--- 2014-04-11/python/exercise2.py
+++ 2014-04-11/python/exercise2.py
@@ -15,7 +15,7 @@ def mkhalfcirc(r):
 
 def walls():
 
-
+    glass = [0.1,0.2,0.3,1, 0,0,0,0.5, 2,2,2,1, 0,0,0,1, 100]
     
     sc1 = PROD([mkhalfcirc(3),QUOTE([3])])
     sc = R([1,2])(PI)(sc1)
@@ -27,16 +27,16 @@ def walls():
     hollows_sud = PROD([QUOTE([0.52,-0.01]*15),QUOTE([1])])
     t_hollows_sud = T([1,2])([0.01,1])(hollows_sud)
     s2 = T(2)(0.5)(R([2,3])(PI/2)(DIFFERENCE([muro_f2_sud,t_hollows_sud])))
+    glass_h_sud = T(2)(0.5)(R([2,3])(PI/2)(MATERIAL(glass)(t_hollows_sud)))
     
-    
-    sud = [s1, T(3)(3)(s2)]
+    sud = [s1, T(3)(3)(s2),T(3)(3)(glass_h_sud)]
     
     
     muro_f1_ovest = PROD([QUOTE([3.5]),QUOTE([3])])
     hollows_f1_o = PROD([QUOTE([1,-0.3]*2),QUOTE([1])])
     t_hollows_f1_o = T([1,2])([0.9,1])(hollows_f1_o)
     o1 = T(2)(-0.1)(T([1,2])([1.1,4.8])(R([1,2])(PI/2)(R([2,3])(PI/2)(DIFFERENCE([muro_f1_ovest,t_hollows_f1_o])))))
-    
+    glass_h_ovest = T(2)(-0.1)(T([1,2])([1.1,4.8])(R([1,2])(PI/2)(R([2,3])(PI/2)(MATERIAL(glass)(t_hollows_f1_o)))))
     
     muro_f2_ovest = PROD([QUOTE([8]),QUOTE([3])])
     hollows_f2_o = PROD([QUOTE([0.45,-0.01]*4),QUOTE([0.7])])
@@ -46,27 +46,30 @@ def walls():
     o2_1 = DIFFERENCE([muro_f2_ovest,t1_hollows_o])
     o2_2 = DIFFERENCE([o2_1,t2_hollows_o])
     o2 = T(2)(0.5)(R([1,2])(PI/2)(R([2,3])(-PI/2)(R([1,2])(PI)(DIFFERENCE([o2_2,t3_hollows_o])))))
+    glass_h_o2 = T(2)(0.5)(R([1,2])(PI/2)(R([2,3])(-PI/2)(R([1,2])(PI)(MATERIAL(glass)(STRUCT([t1_hollows_o,t2_hollows_o,t3_hollows_o]))))))
     
     
-    ovest = [o1,T([1,2,3])([0,8,3])(o2)]
+    ovest = [o1,glass_h_ovest,T([1,2,3])([0,8,3])(o2),T([1,2,3])([0,8,3])(glass_h_o2)]
     
     
     muro_f1_nord = PROD([QUOTE([6]),QUOTE([3])])
     hollow_f1_n = PROD([QUOTE([0.6,-0.02]*7),QUOTE([0.15,-0.01]*10)])
     n1 = T(2)(-0.1)(T([1,2])([7.1,8.3])(R([2,3])(PI/2)(R([1,3])(PI)(DIFFERENCE([muro_f1_nord,T([1,2])([0.8,1])(hollow_f1_n)])))))
+    glass_h_n1 = T(2)(-0.1)(T([1,2])([7.1,8.3])(R([2,3])(PI/2)(R([1,3])(PI)(MATERIAL(glass)(T([1,2])([0.8,1])(hollow_f1_n))))))
     
     muro_f2_nord = PROD([QUOTE([8]),QUOTE([3])])
     hollows_nord = PROD([QUOTE([0.52,-0.01]*15),QUOTE([1])])
     t_hollows_nord = T([1,2])([0.01,1])(hollows_nord)
-    n2 = T(2)(0.5)(T([1,2,3])([8,8,3])(R([2,3])(PI/2)(R([1,3])(PI)(DIFFERENCE([muro_f2_nord,t_hollows_nord])))))
+    n2 = T(2)(0.5)(T([1,2,3])([8,8,3])(R([2,3])(PI/2)(R([1,3])(PI)(DIFFERENCE([muro_f2_nord,t_hollows_nord])))))#
+    glass_h_n2 = T(2)(0.5)(T([1,2,3])([8,8,3])(R([2,3])(PI/2)(R([1,3])(PI)(MATERIAL(glass)(t_hollows_nord)))))
     
-    nord = [n1,n2]
+    nord = [n1,n2,glass_h_n2,glass_h_n1]
     
     muro_f1_est = PROD([QUOTE([3.5]),QUOTE([3])])
     hollows_f1_e = PROD([QUOTE([1,-0.3]*2),QUOTE([1])])
     t_hollows_f1_e = T([1,2])([0.9,1])(hollows_f1_e)
     e1 = T(2)(-0.1)(T([1,2])([7.1,4.8])(R([1,2])(PI/2)(R([2,3])(PI/2)(DIFFERENCE([muro_f1_est,t_hollows_f1_e])))))
-    
+    glass_h_e1 = T(2)(-0.1)(T([1,2])([7.1,4.8])(R([1,2])(PI/2)(R([2,3])(PI/2)(MATERIAL(glass)(t_hollows_f1_e)))))
     
     muro_f2_est = PROD([QUOTE([8]),QUOTE([3])])
     hollows_f2_e = PROD([QUOTE([0.45,-0.01]*4),QUOTE([0.7])])
@@ -75,13 +78,14 @@ def walls():
     t3_hollows_e = T([1,2])([6.1,1])(hollows_f2_e)
     e2_1 = DIFFERENCE([muro_f2_est,t1_hollows_e])
     e2_2 = DIFFERENCE([o2_1,t2_hollows_o])
-    e2 = T(2)(0.5)(T([1,2,3])([8,0,3])(R([1,2])(PI/2)(R([2,3])(PI/2)((DIFFERENCE([o2_2,t3_hollows_o]))))))
-    
-    est = [e1,e2]
+    e2 = T(2)(0.5)(T([1,2,3])([8,0,3])(R([1,2])(PI/2)(R([2,3])(PI/2)((DIFFERENCE([o2_2,t3_hollows_e]))))))
+    glass_h_e2 = T(2)(0.5)(T([1,2,3])([8,0,3])(R([1,2])(PI/2)(R([2,3])(PI/2)(MATERIAL(glass)(STRUCT([t1_hollows_e,t2_hollows_e,t3_hollows_e]))))))
+    est = [e1,e2,glass_h_e2,glass_h_e1]
 
 
 
 
     return sud+ovest+est+nord
 
+COLOR
 #VIEW(STRUCT(walls()))
